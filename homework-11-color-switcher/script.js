ce3be3eafd4353a09c16f5849e93cd87969972d6
Aleksandr Mykhailov <56077587+AleksandrMyk@users.noreply.github.com>
2020-05-29 10:04:58 +0300

    Add files via upload

---
+++ homework-11-color-switcher/script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const refs = {
+  start: document.querySelector('button[data-action="start"]'),
+  stop: document.querySelector('button[data-action="stop"]'),
+  body: document.querySelector("body")
+};
+
+const colors = [
+  "#25468d",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548"
+];
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+const changeColor = {
+  isActive: false,
+
+  start() {
+    if (this.isActive) {
+      return;
+    }
+    this.isActive = true;
+    this.intervalId = setInterval(() => {
+      refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, 5)];
+    }, 1000);
+  },
+  stop() {
+    this.isActive = false;
+    clearInterval(this.intervalId);
+  }
+};
+
+refs.start.addEventListener("click", changeColor.start.bind(changeColor));
+refs.stop.addEventListener("click", changeColor.stop.bind(changeColor));
